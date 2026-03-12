91f7d47f2d065af49c10f87f14afcef5ffed2cba
Anandu Ashok <anandhuasok@gmail.com>
2026-03-12 21:01:49 +0000

    fix main.py

--- app/main.py
+++ app/main.py
@@ -1,4 +1,3 @@
-import os
 from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
@@ -17,11 +16,3 @@ app.include_router(router)
 @app.get("/")
 def home(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
-
-
-if __name__ == "__main__":
-    import uvicorn
-
-    port = int(os.environ.get("PORT", 8000))
-
-    uvicorn.run("app.main:app", host="0.0.0.0", port=port)
